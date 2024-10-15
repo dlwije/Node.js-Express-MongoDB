b0649b1b82d148b0857810022caec73b190528d6
Dinesh Wijethunga <dineshlw8@gmail.com>
2024-10-15 21:33:29 +0400

    :white_check_mark: Section 11 - 165 adding left authorization for login and permission added

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,11 +6,13 @@ const router = express.Router({ mergeParams: true });
 
 // POST /tour/4535gfj/reviews
 // POST /reviews
+
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview,
@@ -19,10 +21,12 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview,
+  )
   .delete(
-    authController.protect,
-    authController.restrictTo('admin'),
+    authController.restrictTo('user', 'admin'),
     reviewController.deleteReview,
   );
 
