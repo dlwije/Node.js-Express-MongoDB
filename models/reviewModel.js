ae19b2ab513daeebac683c2a5ceff4bf1defb1dd
Dinesh Wijethunga <dineshlw8@gmail.com>
2024-10-16 00:21:45 +0400

    :white_check_mark: Section 11 - 168 calculating average ratings when create a review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -47,6 +48,37 @@ reviewSchema.pre(/^find/, function (next){
     select: 'name photo',
   });
   next();
+});
+
+// Static method
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    // 1) Select all the reviews which belongs to passed tourId
+    {
+      $match: { tour: tourId },
+    },
+    // 2) Calculate statistics it self
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  console.log(stats);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating,
+  });
+};
+
+reviewSchema.post('save', function () {
+  // Here 'this' points to the current review
+  this.constructor.calcAverageRatings(this.tour);
+  // We cannot use Review.calcAverageRatings because of it initializes after this middleware.
+  // That's why we use constructor
 });
 
 const Review = mongoose.model('Review', reviewSchema);
