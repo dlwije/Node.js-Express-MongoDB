e2870ebf7061eff0915e52acbaeefbfb962ae683
Dinesh Wijethunga <dineshlw8@gmail.com>
2024-10-19 16:17:18 +0400

    :white_check_mark: Logging out users done

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,10 @@
 /* eslint-disable */
+import axios from "axios";
+import { showAlert } from "./alert";
 
-const login = async (email, password) => {
+export const login = async (email, password) => {
 
-  console.log("login", email, password);
+  // console.log("login", email, password);
   try {
 
     const res = await axios({
@@ -14,20 +16,27 @@ const login = async (email, password) => {
       },
     });
     if (res.data.status === 'success') {
-      alert('Logged In successfully!');
+      showAlert('success','Logged In successfully!');
       window.setTimeout(() => {
         location.assign('/');
       }, 5000);
     }
   }catch (error) {
-    alert(error.response.data.message);
+    showAlert('error',error.response.data.message);
   }
 };
 
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+export const logout = async (email, password) => {
+  try{
+    const res = await axios({
+      method: 'GET',
+      url: 'http://127.0.0.1:3000/api/v1/users/logout',
+    });
 
-  login( email, password );
-})
+    if (res.data.status === 'success') {
+      location.reload(true);
+    }
+  }catch (e) {
+    showAlert('error','Error logging out! Try again!');
+  }
+}
