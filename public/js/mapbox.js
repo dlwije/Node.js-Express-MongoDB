8990cdf780166f4be1675232d25b465203ea59ee
Dinesh Wijethunga <dineshlw8@gmail.com>
2024-10-19 11:32:08 +0400

    :white_check_mark: Section 12 - 186,7 build tour map page done

---
+++ public/js/mapbox.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+ locations = JSON.parse(document.getElementById('map').dataset.locations);
+ map = L.map('map', { zoomControl: false});  //to disable + - zoom
+// var map = L.map('map', { zoomControl: false }).setView([31.111745, -118.113491], );
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  crossOrigin: ""
+}).addTo(map);
+
+points = [];
+locations.forEach((loc) => {
+  points.push([loc.coordinates[1], loc.coordinates[0]]);
+  L.marker([loc.coordinates[1], loc.coordinates[0]])
+    .addTo(map)
+    .bindPopup(`<p>Day ${loc.day}: ${loc.description}</p>`, { autoClose: false })
+    .openPopup();
+});
+
+bounds = L.latLngBounds(points).pad(0.5);
+map.fitBounds(bounds);
+
+map.scrollWheelZoom.disable(); //to disable zoom by mouse wheel
